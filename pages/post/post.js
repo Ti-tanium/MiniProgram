69f06347654e50a619c5625784f3402028a53d5c
Ti-tanium <1158976264@qq.com>
2018-08-05 16:37:22 +0800

    add new post picture

--- pages/post/post.js
+++ pages/post/post.js
@@ -32,6 +32,47 @@ Page({
       collect_count: 96,
       view_img: "/images/View.png",
       view_count: 65
+
+    },
+    {
+      author_img: "/images/user1.jpg",
+      date: "Sep 9 2018",
+      content_img: "/images/fenghuang.jpg",
+      content: "凤凰古城，位于湖南省湘西土家族苗族自治州的西南部，土地总面积约10平方千米。2010年底约5万人口，由苗族、汉族、土家族等28个民族组成，为典型的少数民族聚居区。",
+      collect_img: "/images/Favorites.png",
+      collect_count: 96,
+      view_img: "/images/View.png",
+      view_count: 65
+    },
+    {
+      author_img: "/images/user2.jpg",
+      date: "Sep 8 2018",
+      content_img: "/images/dongjianghu.jpg",
+      content: "东江湖是湖南省唯一一个同时拥有国家AAAAA级旅游区、国家级风景名胜区、国家生态旅游示范区、国家森林公园、国家湿地公园、国家水利风景区“六位一体”的旅游区。",
+      collect_img: "/images/Favorites.png",
+      collect_count: 96,
+      view_img: "/images/View.png",
+      view_count: 65
+    },
+    {
+      author_img: "/images/user3.jpg",
+      date: "Sep 8 2018",
+      content_img: "/images/huashan.jpg",
+      content: "华山是道教主流全真派圣地，为“第四洞天”，也是中国民间广泛崇奉的神祇，即西岳华山君神。共有72个半悬空洞，道观20余座，其中玉泉院、都龙庙、东道院、镇岳宫被列为全国重点道教宫观，有陈抟、郝大通、贺元希等著名的道教高人。",
+      collect_img: "/images/Favorites.png",
+      collect_count: 96,
+      view_img: "/images/View.png",
+      view_count: 65
+    },
+    {
+      author_img: "/images/user4.jpg",
+      date: "Sep 8 2018",
+      content_img: "/images/maerdaifu.jpg",
+      content: "马尔代夫东北与斯里兰卡相距675公里，北部与印度的米尼科伊岛相距约113公里，马尔代夫南部的赤道海峡和一度半海峡为海上交通要道。",
+      collect_img: "/images/Favorites.png",
+      collect_count: 96,
+      view_img: "/images/View.png",
+      view_count: 65
     }
     ]
     this.setData({
