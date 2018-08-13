67b46611018de381324baaa5e48ee42a754ea1ec
Ti-tanium <1158976264@qq.com>
2018-08-13 16:22:07 +0800

    从豆瓣API中请求数据

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -12,7 +12,27 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that=this;
+    wx.request({
+      url: 'http://t.yushu.im/v2/movie/top250',
+      method:'GET',
+      header:{
+        "Content-Type":"application/json"
+      },
+      success:function(res){
+        that.setData({
+          movie_data:res.data
+        })
+      },
+
+      fail:function(error){
+        console.log(error);
+        console.log("fail");
+      },
+
+
+
+    })
   },
 
   /**
